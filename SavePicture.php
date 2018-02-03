92d8a387ee22c1a33ced2cdcc2c7757cb9dd4690
yassine mhed <yassine.mhed@gmail.com>
2018-02-03 15:52:51 -0500

    edit the php script

--- SavePicture.php
+++ SavePicture.php
@@ -5,4 +5,7 @@
 
 	$decoded = base64_decode("$image");
 	file_put_contents("Desktop/".$name."JPG", $decoded);
+
+	$command = escapeshellcmd('/usr/custom/test.py');
+	$output = shell_exec($command);
 ?>
